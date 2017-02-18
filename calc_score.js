831e518b3b22e2d248750a84650efbdba6e6ad07
nms377 <nms377@nyu.edu>
2017-02-18 10:13:32 -1000

    Used the every function method to pass through each index of the array and if each index is less than five 100 points is added to the total score else zero points is added to the total score.

--- calc_score.js
+++ calc_score.js
@@ -1,12 +1,14 @@
+
+
 function scoreThrows (radius) {
 
 	console.log('radius', radius);
 
-	let score = 0;
-
+let score = 0;
 
 		for ( var i = 0; i < radius.length; i++) {	
 
+			//	checks radius and adds score
 			if ( radius[i] > 10 ) {
 				score += 0;
 			} else if ( radius[i] >= 5 && radius[i] <= 10) {
@@ -16,9 +18,20 @@ function scoreThrows (radius) {
 			}
 		}
 
+		if (radius.every(lessThanFive)) {
+			score += 100;
+		} else {
+			score += 0;
+		}
 
 	return score;
 
 }
 
-console.log(scoreThrows([1,2,3,4]));
+function lessThanFive (element, index, array) {
+	return element < 5;
+}
+
+console.log(scoreThrows([1, 2, 3, 4]));
+console.log(scoreThrows([1, 5, 11]));
+console.log(scoreThrows([15, 20, 30]));
